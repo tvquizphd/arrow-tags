1bc58bd4052cea7934edf651c4da1a928f05e342
TVQuizPhD <tvquizphd@gmail.com>
2023-03-21 13:46:24 -0400

    ignore dist

--- dist/index.d.ts
+++
@@ -1,9 +0,0 @@
-declare namespace ArrowTags {
-    function render(id: any, html: any, data: any, input: any): void;
-    function _(pads: any, ...fns: any[]): any[];
-    function Div(...args: any[]): any;
-    function Em(...args: any[]): any;
-    function A(...args: any[]): any;
-}
-
-export { ArrowTags as default };
